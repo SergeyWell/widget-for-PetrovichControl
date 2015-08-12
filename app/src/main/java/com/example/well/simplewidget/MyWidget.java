407a151cdf2c48a5c9b973cc64536f6b4248914a
Well <wolverine13@ukr.net>
2015-08-12 10:17:50 +0300

    fix_block_if

--- app/src/main/java/com/example/well/simplewidget/MyWidget.java
+++ app/src/main/java/com/example/well/simplewidget/MyWidget.java
@@ -9,11 +9,12 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.graphics.Color;
 import android.widget.RemoteViews;
+
 import com.example.widget.R;
 
 
 public class MyWidget extends AppWidgetProvider {
-
+    private static boolean colorType = true;
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager,
@@ -23,19 +24,19 @@ public class MyWidget extends AppWidgetProvider {
             RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.main);
 
             long em = (long) ((System.currentTimeMillis()) / 1000 / 60);
-            long res = (em % 60);
-            String min = "" + res;
+            String min = "" + (em % 60);
 
             remoteViews.setTextViewText(R.id.textView2, min);
 
-            if (res % 2 == 0) {
-                remoteViews.setTextColor(R.id.textView2, Color.parseColor("#FF0F08FF"));
-            } else {
-                remoteViews.setTextColor(R.id.textView2, Color.parseColor("#ffff090c"));
-            }
+        if (colorType) {
+            colorType = false;
+            remoteViews.setTextColor(R.id.textView2, Color.RED);
+        } else {
+            colorType = true;
+            remoteViews.setTextColor(R.id.textView2, Color.BLUE);
+        }
 
-
-            appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
-            }
+        appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
+    }
 
 }
