0da0653d4573ff1fc15e41ce88a1d67b04d72100
Well <wolverine13@ukr.net>
2015-08-13 18:57:12 +0300

    add_bt

--- app/src/main/java/com/example/well/simplewidget/MyWidget.java
+++ app/src/main/java/com/example/well/simplewidget/MyWidget.java
@@ -6,6 +6,7 @@ package com.example.well.simplewidget;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.graphics.Color;
 import android.widget.RemoteViews;
@@ -20,21 +21,32 @@ public class MyWidget extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager,
                          int[] appWidgetIds) {
 
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.main);
+        BluetoothAdapter bt = BluetoothAdapter.getDefaultAdapter();
 
-            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.main);
+        if (!bt.isEnabled()){
+            remoteViews.setImageViewResource(R.id.imageView, R.drawable.ic_error_black_48dp);
+            remoteViews.setTextViewText(R.id.textView2, "NO");
+            remoteViews.setTextColor(R.id.textView2, Color.GRAY);
+
+        } else {
+            remoteViews.setImageViewResource(R.id.imageView, R.drawable.ic_face_black_48dp);
 
             long em = (long) ((System.currentTimeMillis()) / 1000 / 60);
             String min = "" + (em % 60);
 
             remoteViews.setTextViewText(R.id.textView2, min);
 
-        if (colorType) {
-            colorType = false;
-            remoteViews.setTextColor(R.id.textView2, Color.RED);
-        } else {
-            colorType = true;
-            remoteViews.setTextColor(R.id.textView2, Color.BLUE);
+            if (colorType) {
+                colorType = false;
+                remoteViews.setTextColor(R.id.textView2, Color.RED);
+            } else {
+                colorType = true;
+                remoteViews.setTextColor(R.id.textView2, Color.BLUE);
+            }
         }
+
+
 
         appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
     }
